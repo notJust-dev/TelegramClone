e79337614599ccfa5cec21bb70bedb127ba85bf4
Vadim Savin <savinvadim1312@gmail.com>
2024-05-03 18:02:07 +0100

    Start a channel with a user

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React from 'react';
+import { useChatContext } from 'stream-chat-expo';
+import { useAuth } from '../providers/AuthProvider';
+import { router } from 'expo-router';
 
 const UserListItem = ({ user }) => {
+  const { client } = useChatContext();
+  const { user: me } = useAuth();
+
+  const onPress = async () => {
+    //start a chat with him
+    const channel = client.channel('messaging', {
+      members: [me.id, user.id],
+    });
+    await channel.watch();
+    router.replace(`/(home)/channel/${channel.cid}`);
+  };
+
   return (
-    <View style={{ padding: 15, backgroundColor: 'white' }}>
+    <Pressable
+      onPress={onPress}
+      style={{ padding: 15, backgroundColor: 'white' }}
+    >
       <Text style={{ fontWeight: '600' }}>{user.full_name}</Text>
-    </View>
+    </Pressable>
   );
 };
 
