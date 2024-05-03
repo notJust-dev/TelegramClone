697db09c89fe01161a4977397c8bf7fa417f86ea
Vadim Savin <savinvadim1312@gmail.com>
2024-05-03 17:32:36 +0100

    Connect supabase user to streamchat user

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -11,15 +11,18 @@ import { supabase } from '../lib/supabase';
 type AuthContext = {
   session: Session | null;
   user: User | null;
+  profile: any | null;
 };
 
 const AuthContext = createContext<AuthContext>({
   session: null,
   user: null,
+  profile: null,
 });
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [session, setSession] = useState<Session | null>(null);
+  const [profile, setProfile] = useState();
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -31,8 +34,25 @@ export default function AuthProvider({ children }: PropsWithChildren) {
     });
   }, []);
 
+  useEffect(() => {
+    if (!session?.user) {
+      setProfile(null);
+      return;
+    }
+
+    const fetchProfile = async () => {
+      let { data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', session.user.id)
+        .single();
+      setProfile(data);
+    };
+    fetchProfile();
+  }, [session?.user]);
+
   return (
-    <AuthContext.Provider value={{ session, user: session?.user }}>
+    <AuthContext.Provider value={{ session, user: session?.user, profile }}>
       {children}
     </AuthContext.Provider>
   );
--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -2,21 +2,28 @@ import { PropsWithChildren, useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { StreamChat } from 'stream-chat';
 import { Chat, OverlayProvider } from 'stream-chat-expo';
+import { useAuth } from './AuthProvider';
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
 export default function ChatProvider({ children }: PropsWithChildren) {
   const [isReady, setIsReady] = useState(false);
+  const { profile } = useAuth();
 
   useEffect(() => {
+    console.log('USE EFFECT: ', profile);
+    if (!profile) {
+      return;
+    }
     const connect = async () => {
+      console.log(profile.full_name);
       await client.connectUser(
         {
-          id: 'jlahey',
-          name: 'Jim Lahey',
+          id: profile.id,
+          name: profile.full_name,
           image: 'https://i.imgur.com/fR9Jz14.png',
         },
-        client.devToken('jlahey')
+        client.devToken(profile.id)
       );
       setIsReady(true);
 
@@ -29,10 +36,12 @@ export default function ChatProvider({ children }: PropsWithChildren) {
     connect();
 
     return () => {
-      client.disconnectUser();
+      if (isReady) {
+        client.disconnectUser();
+      }
       setIsReady(false);
     };
-  }, []);
+  }, [profile?.id]);
 
   if (!isReady) {
     return <ActivityIndicator />;
